2742a5e17d44f19b0e94524457515ed05d89938c
James-0710 <james_tham1088@hotmail.com>
2024-10-06 18:49:36 +0100

    ned

--- Lecture04/someotherfile.sh
+++ Lecture04/someotherfile.sh
@@ -1,8 +1,4 @@
 Some random text
-<<<<<<< HEAD
 More random text
-||||||| 770bf36
-=======
 This script has achieved total perfection now,
 so I am going to finish things off and call it done.
->>>>>>> Version1.2
